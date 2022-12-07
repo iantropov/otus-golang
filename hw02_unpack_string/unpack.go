89cc368c8238e651562fd468fb9a626145683ed5
antropov-ivan <antropov-ivan@yandex-team.ru>
2022-12-07 11:00:17 +0600

    Распаковка строки (wip)

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,73 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+type Mode int
+
+const (
+	ValueMode Mode = iota
+	QuantityMode
+	ErrorMode
+)
+
+type State struct {
+	mode        Mode
+	currentRune rune
+	result      strings.Builder
+}
+
+func Unpack(value string) (string, error) {
+	state := emptyState()
+	for _, r := range value {
+		error := state.processRune(r)
+		if error != nil {
+			return "", error
+		}
+	}
+	return state.getResult(), nil
+}
+
+func emptyState() *State {
+	return &State{
+		mode: ValueMode,
+	}
+}
+
+func (state *State) processRune(r rune) error {
+	if state.mode == ValueMode {
+		if isValidValue(r) {
+			state.currentRune = r
+			state.mode = QuantityMode
+		} else {
+			return ErrInvalidString
+		}
+	} else {
+		if isValidQuantity(r) {
+			state.expandResult()
+			state.mode = ValueMode
+		} else {
+			return ErrInvalidString
+		}
+	}
+	return nil
+}
+
+func (state *State) expandResult() {
+	state.result.WriteString(strings.Repeat(string(state.currentRune), 5))
+}
+
+func (state *State) getResult() string {
+	return state.result.String()
+}
+
+func isValidValue(value rune) bool {
+	return true
+}
+
+func isValidQuantity(value rune) bool {
+	return true
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -1,9 +1,10 @@
-package hw02unpackstring
+package hw02unpackstring_test
 
 import (
 	"errors"
 	"testing"
 
+	hw02unpackstring "github.com/iantropov/otus-lang/hw02_unpack_string"
 	"github.com/stretchr/testify/require"
 )
 
@@ -26,7 +27,7 @@ func TestUnpack(t *testing.T) {
 	for _, tc := range tests {
 		tc := tc
 		t.Run(tc.input, func(t *testing.T) {
-			result, err := Unpack(tc.input)
+			result, err := hw02unpackstring.Unpack(tc.input)
 			require.NoError(t, err)
 			require.Equal(t, tc.expected, result)
 		})
@@ -38,8 +39,8 @@ func TestUnpackInvalidString(t *testing.T) {
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
-			_, err := Unpack(tc)
-			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
+			_, err := hw02unpackstring.Unpack(tc)
+			require.Truef(t, errors.Is(err, hw02unpackstring.ErrInvalidString), "actual error %q", err)
 		})
 	}
 }
