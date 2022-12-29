3c32400ba077e08434e32e021fca809b6d30246e
antropov-ivan <antropov-ivan@yandex-team.ru>
2022-12-29 11:38:41 +0600

    Реализация кэша

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,8 +9,6 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
@@ -27,4 +25,44 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (lc *lruCache) Set(key Key, value interface{}) bool {
+	i, exists := lc.items[key]
+	if exists {
+		lc.queue.MoveToFront(i)
+		cacheItem := i.Value.(cacheItem)
+		cacheItem.value = value
+		return true
+	}
+
+	newCacheItem := &cacheItem{key, value}
+	i = lc.queue.PushFront(newCacheItem)
+	if lc.queue.Len() > lc.capacity {
+		back := lc.queue.Back()
+		backCacheItem := back.Value.(cacheItem)
+
+		lc.queue.Remove(back)
+		delete(lc.items, backCacheItem.key)
+	}
+
+	lc.items[key] = i
+
+	return false
+}
+
+func (lc *lruCache) Get(key Key) (interface{}, bool) {
+	i, exists := lc.items[key]
+	if !exists {
+		return nil, false
+	}
+
+	lc.queue.MoveToFront(i)
+	cacheItem := i.Value.(cacheItem)
+	return cacheItem.value, true
+}
+
+func (lc *lruCache) Clear() {
+	lc.queue = NewList()
+	lc.items = make(map[Key]*ListItem, lc.capacity)
 }
