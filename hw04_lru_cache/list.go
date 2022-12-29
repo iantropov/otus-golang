3abf4c4b844999716a711b57aa30657e6d88d2fe
antropov-ivan <antropov-ivan@yandex-team.ru>
2022-12-29 11:10:44 +0600

    Реализация списка

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,10 +17,72 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	head, tail *ListItem
+	len        int
 }
 
 func NewList() List {
 	return new(list)
+}
+
+func (l *list) Len() int {
+	return l.len
+}
+
+func (l *list) Front() *ListItem {
+	return l.head
+}
+
+func (l *list) Back() *ListItem {
+	return l.tail
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	newItem := new(ListItem)
+	l.pushFront(newItem)
+	return l.head
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	newItem := new(ListItem)
+	if l.tail != nil {
+		l.tail.Next = newItem
+	}
+
+	newItem.Value = v
+	newItem.Prev = l.tail
+	l.tail = newItem
+
+	return l.tail
+}
+
+func (l *list) Remove(i *ListItem) {
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	} else {
+		l.head = i.Next
+	}
+
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	} else {
+		l.tail = i.Prev
+	}
+
+	i.Next = nil
+	i.Prev = nil
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	l.Remove(i)
+	l.pushFront(i)
+}
+
+func (l *list) pushFront(i *ListItem) {
+	if l.head != nil {
+		l.head.Prev = i
+	}
+
+	i.Next = l.head
+	l.head = i
 }
