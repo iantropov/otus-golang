95b88f38d3b57377364a27288783ac95d3dec404
antropov-ivan <antropov-ivan@yandex-team.ru>
2023-01-20 09:03:55 +0600

    Реализация планировщика

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -10,6 +10,47 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
-	return nil
+	okChannel := make(chan bool)
+	errorsCounter := 0
+	runningTasks := 0
+	tasksPointer := 0
+
+	for i := 0; i < n; i++ {
+		go doTask(tasks[tasksPointer], okChannel)
+		runningTasks++
+		tasksPointer++
+	}
+
+	var err error
+	for isFinished := false; !isFinished; {
+		select {
+		case ok := <-okChannel:
+			if !ok {
+				errorsCounter++
+			}
+
+			if errorsCounter > m {
+				err = ErrErrorsLimitExceeded
+			}
+
+			runningTasks--
+			if runningTasks == 0 && (tasksPointer == len(tasks) || errorsCounter > m) {
+				isFinished = true
+				break
+			}
+
+			if runningTasks < n && errorsCounter <= m && tasksPointer < len(tasks) {
+				go doTask(tasks[tasksPointer], okChannel)
+				tasksPointer++
+				runningTasks++
+			}
+		}
+	}
+
+	return err
+}
+
+func doTask(task Task, okChannel chan<- bool) {
+	err := task()
+	okChannel <- err == nil
 }
