90fd84da0d1ba4e8cddefc5bfaa90d4a0800dd86
antropov-ivan <antropov-ivan@yandex-team.ru>
2023-01-30 08:53:24 +0600

    Упростил решение

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,6 +1,7 @@
 package hw06pipelineexecution
 
 import (
+	"runtime"
 	"sync"
 )
 
@@ -18,95 +19,47 @@ func ExecutePipeline(in In, done In, stages ...Stage) Out {
 		close(out)
 		return out
 	}
-
-	isDone := make(chan struct{}, 1)
-	outs := make([]Bi, 0)
-
-	wgPipeline := sync.WaitGroup{}
-	wgMaster := sync.WaitGroup{}
-	wgWorkers := sync.WaitGroup{}
-	wgMaster.Add(1)
-	wgPipeline.Add(1)
-
 	stagesWithDone := make([]Stage, 0, len(stages))
 	for i := 0; i < len(stages); i++ {
 		stagesWithDone = append(stagesWithDone, stageWithDone(done, stages[i]))
 	}
-
+	numPipes := runtime.NumCPU()
+	wg := sync.WaitGroup{}
+	wg.Add(numPipes)
+	for i := 0; i < numPipes; i++ {
+		go pipeline(in, done, stagesWithDone, out, &wg)
+	}
 	go func() {
-		defer wgMaster.Done()
-		for vIn := range in {
-			nextOut := make(Bi, 1)
-			outs = append(outs, nextOut)
-
-			wgWorkers.Add(1)
-			go pipelineWorker(vIn, stagesWithDone, nextOut, &wgWorkers)
-		}
+		wg.Wait()
+		close(out)
 	}()
-
-	go func() {
-		wgMaster.Wait()
-		wgWorkers.Wait()
-		select {
-		case <-done:
-			isDone <- struct{}{}
-		default:
-		}
-		wgPipeline.Done()
-	}()
-
-	go func() {
-		defer func() {
-			close(isDone)
-			close(out)
-		}()
-
-		wgPipeline.Wait()
-
-		select {
-		case <-isDone:
-			return
-		default:
-		}
-
-		for _, nextOut := range outs {
-			out <- <-nextOut
-			close(nextOut)
-		}
-	}()
-
 	return out
 }
 
-func pipelineWorker(vIn interface{}, stages []Stage, out Bi, wg *sync.WaitGroup) {
+func pipeline(in In, done In, stages []Stage, out Bi, wg *sync.WaitGroup) {
 	defer wg.Done()
-	nextIn := make(Bi, 1)
-	nextIn <- vIn
-	vOut := passStages(nextIn, stages)
-	if vOut != nil {
-		out <- vOut
+	stageOut := stages[0](in)
+	for i := 1; i < len(stages); i++ {
+		stageOut = stages[i](stageOut)
 	}
-}
-
-func passStages(in In, stages []Stage) interface{} {
-	nextIn := in
-	var lastOut Out
-	for _, stage := range stages {
-		lastOut = stage(nextIn)
-		nextIn = lastOut
+	for {
+		select {
+		case vOut := <-stageOut:
+			if vOut == nil {
+				return
+			}
+			out <- vOut
+		case <-done:
+			return
+		}
 	}
-	return <-lastOut
 }
 
 func stageWithDone(done In, stage Stage) Stage {
 	return func(in In) (out Out) {
 		stageIn := make(Bi)
-
 		go func() {
-			defer func() {
-				close(stageIn)
-			}()
-
+			defer close(stageIn)
 			for {
 				select {
 				case vIn := <-in:
@@ -119,7 +72,6 @@ func stageWithDone(done In, stage Stage) Stage {
 				}
 			}
 		}()
-
 		return stage(stageIn)
 	}
 }
