87fd9b0f17e7196c9245bc72b6ebf88994e07579
antropov-ivan <antropov-ivan@yandex-team.ru>
2023-02-02 08:28:32 +0600

    Переделал решение

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,7 +1,6 @@
 package hw06pipelineexecution
 
 import (
-	"runtime"
 	"sync"
 )
 
@@ -23,11 +22,10 @@ func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	for i := 0; i < len(stages); i++ {
 		stagesWithDone = append(stagesWithDone, stageWithDone(done, stages[i]))
 	}
-	numPipes := runtime.NumCPU()
 	wg := sync.WaitGroup{}
-	wg.Add(numPipes)
-	for i := 0; i < numPipes; i++ {
-		go pipeline(in, done, stagesWithDone, out, &wg)
+	for vIn := range in {
+		wg.Add(1)
+		go pipeline(vIn, done, stagesWithDone, out, &wg)
 	}
 	go func() {
 		wg.Wait()
@@ -36,22 +34,21 @@ func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	return out
 }
 
-func pipeline(in In, done In, stages []Stage, out Bi, wg *sync.WaitGroup) {
+func pipeline(vIn interface{}, done In, stages []Stage, out Bi, wg *sync.WaitGroup) {
 	defer wg.Done()
+	in := make(Bi, 1)
+	defer close(in)
+	in <- vIn
 	stageOut := stages[0](in)
 	for i := 1; i < len(stages); i++ {
 		stageOut = stages[i](stageOut)
 	}
-	for {
-		select {
-		case vOut := <-stageOut:
-			if vOut == nil {
-				return
-			}
+	select {
+	case vOut := <-stageOut:
+		if vOut != nil {
 			out <- vOut
-		case <-done:
-			return
 		}
+	case <-done:
 	}
 }
 
