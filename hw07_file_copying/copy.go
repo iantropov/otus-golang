95320bcb8a5ca1b39eef415d4a45bfbae21074db
antropov-ivan <antropov-ivan@yandex-team.ru>
2023-02-09 09:18:23 +0600

    Реализовал утилиту копирования файлов

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,87 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrEmptyFromFileName     = errors.New("empty from filename")
+	ErrEmptyToFileName       = errors.New("empty to filename")
+	ErrFromFileNotFound      = errors.New("from file not found")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if from == "" {
+		return ErrEmptyFromFileName
+	}
+	if to == "" {
+		return ErrEmptyToFileName
+	}
+
+	fileStat, err := os.Stat(from)
+	if err != nil {
+		panic(err)
+	}
+	size := fileStat.Size()
+	if size == 0 {
+		return ErrUnsupportedFile
+	}
+	if offset >= size {
+		return ErrOffsetExceedsFileSize
+	}
+
+	var inputFile *os.File
+	inputFile, err = os.Open(from)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return ErrFromFileNotFound
+		} else {
+			return err
+		}
+	}
+	defer inputFile.Close()
+
+	var outputFile *os.File
+	outputFile, err = os.OpenFile(to, os.O_RDWR|os.O_CREATE|os.O_TRUNC, fileStat.Mode())
+	if err != nil {
+		return err
+	}
+	defer outputFile.Close()
+
+	if limit == 0 {
+		limit = size
+	}
+
+	bufferSize := limit
+	if bufferSize > CHUNK_SIZE {
+		bufferSize = CHUNK_SIZE
+	}
+	buffer := make([]byte, bufferSize)
+
+	curPos := offset
+	for curPos < offset+limit {
+		read, readErr := inputFile.ReadAt(buffer, curPos)
+		if readErr != nil && readErr != io.EOF {
+			return readErr
+		}
+		if curPos+int64(read) > offset+limit {
+			read = int(offset + limit - curPos)
+		}
+		written := 0
+		for written < read {
+			nextWritten, writeErr := outputFile.Write(buffer[written:read])
+			if writeErr != nil {
+				return writeErr
+			}
+			written += nextWritten
+		}
+		if readErr == io.EOF {
+			break
+		}
+		curPos += int64(read)
+	}
 	return nil
 }
