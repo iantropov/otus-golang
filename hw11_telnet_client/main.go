2267e393c363e73cd5f4c5c544cc59e7282f3bcb
antropov-ivan <antropov-ivan@yandex-team.ru>
2023-03-15 14:22:05 +0600

    Реализация клиента (WIP)

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,133 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"time"
+)
+
+var timeout time.Duration
+
+func init() {
+	flag.DurationVar(&timeout, "timeout", 10*time.Second, "connection timeout")
+}
+
+type MyBuffer struct {
+	bytes.Buffer
+	onClose func()
+}
+
+func NewMyBuffer(onClose func()) *MyBuffer {
+	return &MyBuffer{
+		onClose: onClose,
+	}
+}
+
+func (mb *MyBuffer) Close() error {
+	mb.onClose()
+	return nil
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	// if len(os.Args) != 3 {
+	// 	fmt.Println("Please, provide host and port arguments")
+	// 	return
+	// }
+
+	// host := os.Args[1]
+	// port := os.Args[2]
+
+	host := "localhost"
+	port := "9000"
+
+	connectionStr := fmt.Sprintf("%s:%s", host, port)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	bufferIn := NewMyBuffer(func() {
+		fmt.Println("Received close from the server!")
+		cancel()
+	})
+	bufferOut := &bytes.Buffer{}
+
+	telnetClient := NewTelnetClient(connectionStr, timeout, bufferIn, bufferOut)
+	err := telnetClient.Connect()
+	if err != nil {
+		log.Fatalf("failed to connect to: %v\n", err)
+	}
+	defer telnetClient.Close()
+
+	wg := sync.WaitGroup{}
+
+	wg.Add(1)
+	go func() {
+		defer cancel()
+		defer wg.Done()
+
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Println("Ready to Scan!")
+		for scanner.Scan() {
+			if ctx.Err() != nil {
+				fmt.Println("Received ctx.Done() in sender")
+				return
+			}
+			fmt.Println("Scanned!")
+
+			bufferIn.Reset()
+			bufferIn.Write(scanner.Bytes())
+			bufferIn.WriteString("\n")
+			err := telnetClient.Send()
+			if err != nil {
+				fmt.Println("Failed to send data to the telnet client", err)
+				return
+			}
+		}
+		fmt.Println("Finished scanning!")
+
+		if scanner.Err() != io.EOF {
+			fmt.Println("Received error from scanner", scanner.Err())
+			return
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer cancel()
+		defer wg.Done()
+
+		fmt.Println("Ready to receive!")
+		for {
+			if ctx.Err() != nil {
+				fmt.Println("Received ctx.Done() in receiver")
+				return
+			}
+			fmt.Println("Will receive!")
+
+			err := telnetClient.Receive()
+			fmt.Print("Received: ")
+			if err != nil {
+				fmt.Println("Failed to receive data from the telnet client", err)
+				return
+			}
+			_, err = os.Stdout.Write(bufferOut.Bytes())
+			if err != nil {
+				fmt.Println("Failed to send data to STDOUT", err)
+			}
+			os.Stdout.WriteString("\n")
+			bufferOut.Reset()
+		}
+	}()
+
+	wg.Wait()
+
+	fmt.Println("FINISHED")
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,10 +14,47 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type TelnetClientImpl struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
 	// Place your code here.
-	return nil
+	return &TelnetClientImpl{address, timeout, in, out, nil}
 }
 
 // Place your code here.
 // P.S. Author's solution takes no more than 50 lines.
+
+func (tc *TelnetClientImpl) Connect() error {
+	conn, err := net.DialTimeout("tcp", tc.address, tc.timeout)
+	tc.conn = conn
+	return err
+}
+
+func (tc *TelnetClientImpl) Close() error {
+	return tc.conn.Close()
+}
+
+func (tc *TelnetClientImpl) Send() error {
+	data, err := io.ReadAll(tc.in)
+	if err != nil {
+		return err
+	}
+	_, err = tc.conn.Write(data)
+	return err
+}
+
+func (tc *TelnetClientImpl) Receive() error {
+	scanner := bufio.NewScanner(tc.conn)
+	ok := scanner.Scan()
+	if ok {
+		_, err := tc.out.Write(scanner.Bytes())
+		return err
+	}
+	return scanner.Err()
+}
