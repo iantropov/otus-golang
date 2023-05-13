1de58ba4ece1b0d57e5fac3023160020759bf8a1
antropov-ivan <antropov-ivan@yandex-team.ru>
2023-05-13 12:37:42 +0600

    Получение строк подключения к rabbit и postgres

--- hw12_13_14_15_calendar/functest/main_test.go
+++ hw12_13_14_15_calendar/functest/main_test.go
@@ -1,6 +1,8 @@
 package functest
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -11,7 +13,21 @@ import (
 // 	os.Exit(m.Run())
 // }
 
+var amqpDSN = os.Getenv("TESTS_AMQP_DSN")
+var postgresDSN = os.Getenv("TESTS_POSTGRES_DSN")
+
+func init() {
+	if amqpDSN == "" {
+		amqpDSN = "amqp://guest:guest@localhost:5672/"
+	}
+	if postgresDSN == "" {
+		postgresDSN = "host=localhost port=5432 user=calendar password=password dbname=calendar sslmode=disable"
+	}
+}
+
 func TestTest(t *testing.T) {
+	fmt.Println("Hello, AMQP", amqpDSN)
+	fmt.Println("Hello, POSTGRES", postgresDSN)
 	t.Run("hello world", func(t *testing.T) {
 		require.Equal(t, true, false)
 	})
